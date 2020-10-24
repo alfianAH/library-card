dd37bbbaae3ced87c6684ee28858350557588f3d
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-10-24 22:27:14 +0800

    Fix bug when adding same book

--- user/book/add_book.php
+++ user/book/add_book.php
@@ -38,20 +38,30 @@ FROM buku_tbl, peminjaman_tbl
 WHERE peminjaman_tbl.nim = '" . $nim . "' AND
 peminjaman_tbl.isbn = buku_tbl.isbn");
 
+            $is_book_same = false;
+
             // Check user's books
             while ($row = mysqli_fetch_assoc($bookuser)) {
                 $i++;
+
+                // Check same book in user
+                if($isbn == $row['isbn']) {
+                    $is_book_same = true;
+                    $_SESSION['notification'] = "Buku sudah dipinjam";
+                    header("Location: ../list_library_book.php");
+                }
             }
 
-            if ($i < 3) {
+            if ($i < 3 && !$is_book_same) {
                 $result = $db->execute("INSERT INTO peminjaman_tbl(nim, isbn, waktu_peminjaman, waktu_pengembalian, denda) 
 VALUES('" . $nim . "', " . $isbn . ", CURRENT_DATE(), CURRENT_DATE() + 7, 0)");
 
                 if($result) {
+                    echo "jalan";
                     $_SESSION['notification'] = "Buku berhasil ditambahkan.";
                     header("Location: ../list_user_book.php");
                 }
-            } else{
+            } else if($i > 3){
                 $_SESSION['notification'] = "Batas peminjaman buku adalah 3 buku.";
                 header("Location: ../list_library_book.php");
             }
